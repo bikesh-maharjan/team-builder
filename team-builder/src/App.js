ca9c2ae15dad5be1947722a958793f3b09591ace
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-07-21 16:22:33 -0700

    Form Completed

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Form from "./Form";
+import Team from "./Team";
+
+const initialTeamList = [
+  {
+    name: " ",
+    email: " ",
+    role: " ",
+  },
+];
+
+const fakeAxiosGet = () => {
+  return Promise.resolve({ status: 200, success: true, data: initialTeamList });
+};
+
+const fakeAxiosPost = ({ name, email, role }) => {
+  const newTeamList = { name, email, role };
+
+  return Promise.resolve({ status: 200, success: true, data: newTeamList });
+};
 
 function App() {
+  const [teams, setTeams] = useState([]);
+
+  const [formValues, setFormValues] = useState(initialTeamList);
+
+  const updateList = (inputName, inputValue) => {
+    const updatedList = { ...formValues, [inputName]: inputValue };
+    setFormValues(updatedList);
+  };
+
+  const submitForm = () => {
+    const newTeamList = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    };
+
+    if (!newTeamList.name || !newTeamList.email || !newTeamList.role) return;
+
+    fakeAxiosPost("fakeapi.com", newTeamList).then((res) => {
+      const teamFromAPI = res.data;
+      setTeams([teamFromAPI, ...teams]);
+      setFormValues(initialTeamList);
+    });
+  };
+  useEffect(() => {
+    fakeAxiosGet("fakeapi.com").then((res) => setTeams(res.data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Team Builder List</h1>
+      <Form values={formValues} update={updateList} submit={submitForm} />
+      {teams.map((team) => {
+        return <Team key={teams.id} details={team} />;
+      })}
     </div>
   );
 }
